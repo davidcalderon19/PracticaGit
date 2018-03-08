503706b8a7a1915bcf9921c2b0cbd65c8843bcfe
user <user@com2pc44>
2018-03-08 05:37:03 -0600

    primer commit

---
+++ src/proyectogit/ProyectoGit.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package proyectogit;
+
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author user
+ */
+public class ProyectoGit {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        JOptionPane.showMessageDialog(null," que ondas perro");
+    }
+    
+}
